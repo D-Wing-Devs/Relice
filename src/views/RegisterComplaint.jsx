69ea5819de8fcabd8daeef7d7e719599891d6134
Sourabhtripathi <sourabh.tripathi48@gmail.com>
2019-10-19 02:15:33 +0530

    add registration button styling and alert

--- src/views/RegisterComplaint.jsx
+++ src/views/RegisterComplaint.jsx
@@ -5,9 +5,25 @@ import { selectImage } from '../actions';
 import { connect } from 'react-redux';
 
 class RegisterComplaint extends Component {
+	state = {
+		alert: false
+	};
+
 	handleFileChange = (event) => {
 		console.log(event.currentTarget.files[0]);
 		this.props.selectImage(URL.createObjectURL(event.currentTarget.files[0]));
+	};
+
+	handleSubmit = () => {
+		if (this.props.selectedImage === null) {
+			this.setState({
+				alert: true
+			});
+		} else {
+			this.setState({
+				alert: false
+			});
+		}
 	};
 
 	render() {
@@ -21,31 +37,44 @@ class RegisterComplaint extends Component {
 						</div>
 					}
 				/>
-				<div className="header text-center" style={{ margin: '2% auto' }}>
-					<img
-						src={this.props.selectedImage ? this.props.selectedImage : defaultImage}
-						alt="uploaded-image"
-						style={{ height: '200px', width: '200px' }}
-					/>
-					<input
-						required
-						type="file"
-						onChange={this.handleFileChange}
-						ref={(input) => (this.inputElement = input)}
-						style={{ display: 'none' }}
-					/>
-					<div className="form-group form-control" style={{ border: 'none' }}>
-						<button
-							className="btn btn-primary"
-							onClick={(e) => {
-								e.preventDefault();
-								this.inputElement.click();
+				<form>
+					<div className="header text-center" style={{ margin: '2% auto' }}>
+						<img
+							src={this.props.selectedImage ? this.props.selectedImage : defaultImage}
+							alt="uploaded-image"
+							style={{ height: '200px', width: '200px' }}
+						/>
+						<div
+							class="alert alert-danger"
+							role="alert"
+							style={{
+								width: '20%',
+								fontSize: '13px',
+								margin: '2% auto',
+								display: this.state.alert ? 'block' : 'none'
 							}}
 						>
-							<p style={{ margin: '0', padding: '0' }}>Pick an Image</p>
-						</button>
-					</div>
-					<form onSubmit={this.handleSubmit}>
+							Please Upload the Image
+						</div>
+						<input
+							type="file"
+							onChange={this.handleFileChange}
+							ref={(input) => (this.inputElement = input)}
+							style={{ display: 'none' }}
+							required
+						/>
+						<div className="form-group form-control" style={{ border: 'none' }}>
+							<button
+								className="btn btn-primary"
+								onClick={(e) => {
+									e.preventDefault();
+									this.inputElement.click();
+								}}
+							>
+								<p style={{ margin: '0', padding: '0' }}>Pick an Image</p>
+							</button>
+						</div>
+
 						<div
 							className="form-group form-control"
 							style={{ background: 'white', padding: '3%', width: '75%', margin: '0 auto' }}
@@ -65,7 +94,6 @@ class RegisterComplaint extends Component {
 										id="colFormLabelSm"
 										name="name"
 										placeholder="Name"
-								
 									/>
 								</div>
 								<label htmlFor="colFormLabelSm" className="col-sm-2 col-form-label col-form-label-sm">
@@ -169,9 +197,11 @@ class RegisterComplaint extends Component {
 								</div>
 							</div>
 						</div>
-						<input type="submit"/>
-					</form>
-				</div>
+						<button className="btn btn-info" onClick={this.handleSubmit}>
+							Submit
+						</button>
+					</div>
+				</form>
 			</Fragment>
 		);
 	}
